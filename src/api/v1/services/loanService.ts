1ded97b9e9cebb9a5523b4d9147b160cbdd9e5f6
August <mturzak@rrc.ca>
2026-03-11 18:10:10 -0500

    created loanService CRUD logic

---
+++ src/api/v1/services/loanService.ts
@@ -0,0 +1,29 @@
+import { addLoan, getLoanById, getLoans, updateLoans, deleteLoan } from "../repositories/firestoreRepository";
+import { LoanReponse } from "../models/loanResponse";
+import { LoanCreateRequest } from "../models/loanCreateRequestModel";
+import { LoanDTO } from "../models/loanDTO";
+
+export const createNewLoan =  async (item: LoanCreateRequest): Promise<string> => {
+    return await addLoan(item); 
+}
+
+export const getLoanByIdAsync = async (id: string): Promise<LoanReponse> => {
+    let entity = await getLoanById(id);
+    return {
+        id: entity?.id,
+        applicant: entity?.applicant
+    };
+}
+
+export const getAllLoans = async (): Promise<Array<LoanDTO> | undefined> => {
+    return await getLoans();
+}
+
+export const updateLoanById = async (id: string, item: LoanCreateRequest): Promise<void> => {
+    await updateLoans(id, item);
+    return;
+}
+
+export const deleteLoanById = async (id: string): Promise<void> => {
+    await deleteLoan(id)
+}
