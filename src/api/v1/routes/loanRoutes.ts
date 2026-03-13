1b214bcb776c250dda564ad3e38befec0c040f84
August <mturzak@rrc.ca>
2026-03-13 18:08:04 -0500

    added manager access to post and put endpoints

--- src/api/v1/routes/loanRoutes.ts
+++ src/api/v1/routes/loanRoutes.ts
@@ -15,8 +15,8 @@ const loanRouter: Router = express.Router();
 loanRouter.get("/health", healthData);
 loanRouter.get("/loans", authenticate, isAuthorized({ hasRole: ["admin", "officer"], allowSameUser: true}), getAllLoan);
 loanRouter.get("/loans/:id", authenticate, validateRequest(postSchemas.getById), getLoanById);
-loanRouter.post("/loans", authenticate, isAuthorized({ hasRole: ["admin"], allowSameUser: true}), validateRequest(postSchemas.create), createLoan);
-loanRouter.put("/loans/:id", authenticate, isAuthorized({ hasRole: ["admin"], allowSameUser: true}),validateRequest(postSchemas.update), updateLoanByIdAsync);
+loanRouter.post("/loans", authenticate, isAuthorized({ hasRole: ["admin", "manager"], allowSameUser: true}), validateRequest(postSchemas.create), createLoan);
+loanRouter.put("/loans/:id", authenticate, isAuthorized({ hasRole: ["admin", "manager"], allowSameUser: true}),validateRequest(postSchemas.update), updateLoanByIdAsync);
 loanRouter.delete("/loans/:id", authenticate, isAuthorized({ hasRole: ["admin"], allowSameUser: true}),validateRequest(postSchemas.delete), deleteLoanByIdAsync);
 
 export default loanRouter;
